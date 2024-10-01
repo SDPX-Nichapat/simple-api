e837d12a41d60170fcab497136f09879a06dd201
Horii <nichapat.uah@gmail.com>
2024-10-01 14:06:42 +0700

    edit test_unit

--- test_unit.py
+++ test_unit.py
@@ -47,15 +47,15 @@ import app
 class TestApp(unittest.TestCase):
 
     def test_true_when_x_is_17(self):
-        response = app.is_prime(17)
+        response = app.prime(17)
         self.assertEqual(response, "true")
 
     def test_false_when_x_is_36(self):
-        response = app.is_prime(36)
+        response = app.prime(36)
         self.assertEqual(response, "false")
 
     def test_true_when_x_is_13219(self):
-        response = app.is_prime(13219)
+        response = app.prime(13219)
         self.assertEqual(response, "true")
 
 if __name__ == "__main__":
